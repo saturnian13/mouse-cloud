047d391533ca118c43d40d689153c0a3787a5461
Jason Patterson <jpatterson7398@gmail.com>
2016-10-17 07:47:44 -0400

    Began making rewards plot

--- runner/views.py
+++ runner/views.py
@@ -5,9 +5,10 @@ from django.template import RequestContext
 from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render_to_response
 from .models import Session
+from .models import Box
 
 import pandas
-import datetime
+from datetime import date, timedelta
 
 # I think there's a thread problem with importing pyplot here
 # Maybe if you specify matplotlib.use('Agg') it would be okay
@@ -63,22 +64,59 @@ def weight_plot(request):
 
 def rewards_plot(request):
     boxes = Box.objects.all()
-    f = Figure()
+    f = Figure(figsize=(12, 20), dpi=80)
+
     axes = [f.add_subplot(len(boxes), 1, n+1) for n in range(len(boxes))]
+    f.subplots_adjust(top=.95, bottom=.1, hspace=.4)
+    f.set_facecolor('w')
 
     for box, ax in zip(boxes, axes):
-        ax.set_title(box.name)
-        ax.set_ylabel('Water Volume (uL)')
-
         
 
-        # for box, ax
-        # for session in Session.objects.filter(box=box, date_time_start__lte=datetime.today)
 
-    canvas + FigureCanvas(f)
+        sessions = Session.objects.filter(box=box, date_time_start__gte = date.today() - timedelta(days=60))
+        dates = sessions.datetimes('date_time_start', 'day')
+
+        left_volumes = []
+        right_volumes = []
+
+        for d in dates:
+            sessions_by_date = sessions.filter(date_time_start__date = d)
+
+            left_volume = sum([session.user_data_left_water_consumption for session in sessions_by_date]) / float(len(sessions_by_date))
+            right_volume = sum([session.user_data_right_water_consumption for session in sessions_by_date]) / float(len(sessions_by_date))
+
+            left_volumes.append(left_volume)
+            right_volumes.append(right_volume)
+
+        if dates:
+
+            ax.plot(dates, left_volumes, 'bo-')
+            ax.plot(dates, right_volumes, 'ro-')
+
+            date_range = max(dates) - min(dates)
+            labels = [(min(dates) + timedelta(days=i)).strftime("%b %d") for i in range(date_range.days + 1) ]
+
+
+            ax.set_title(box.name)
+            ax.set_ylabel('Water Volume (uL)')
+
+            ax.set_xticks(range(len(labels)))
+            ax.set_xticklabels(labels, rotation=45, size='medium')
+
+            ax.set_xlim(min(dates), max(dates))
+
+            ax.legend(['Left Pipe', 'Right Pipe'], loc='upper right', fontsize='medium')
+
+    canvas = FigureCanvas(f)
     response = HttpResponse(content_type='image/png')
     canvas.print_png(response)
     return response
+
+
+
+# for box, ax
+# for session in Session.objects.filter(box=box, date_time_start__lte=datetime.today)
     
 
 
