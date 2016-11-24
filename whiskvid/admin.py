e6340e84aac07814e51649ddc44338013433fe7e
Chris Rodgers <xrodgers@gmail.com>
2016-11-23 19:36:19 -0500

    adding to admin site

--- whiskvid/admin.py
+++ whiskvid/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+import whiskvid.models
 
 # Register your models here.
+class VideoSessionAdmin(admin.ModelAdmin):
+    list_display = ['name', 'bsession',]
+
+admin.site.register(whiskvid.models.VideoSession, VideoSessionAdmin)
