674bb968b152342621b19d5f40ec9129acc78a4f
Chris Rodgers <xrodgers@gmail.com>
2016-11-25 20:26:16 -0500

    adding admin for neural sessions

--- neural_sessions/admin.py
+++ neural_sessions/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+import neural_sessions.models
 
 # Register your models here.
+class NeuralSessionAdmin(admin.ModelAdmin):
+    list_display = ['name', 'sort_name', 'bsession',]
+
+admin.site.register(neural_sessions.models.NeuralSession, NeuralSessionAdmin)
