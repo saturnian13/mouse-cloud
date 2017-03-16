f88487f2f58244af823da90ac495712ad5c9e4c9
Chris Rodgers <xrodgers@gmail.com>
2017-03-16 14:58:35 -0400

    adding stepper offset steps C parameter to board

--- runner/models.py
+++ runner/models.py
@@ -176,6 +176,7 @@ class Board(models.Model):
     microstep = models.CharField(max_length=10, null=True, blank=True)
     invert_stepper_direction = models.CharField(max_length=10, 
         null=True, blank=True)
+    stepper_offset_steps = models.CharField(max_length=10, default='0')
     
     def __str__(self):
         return str(self.name)
