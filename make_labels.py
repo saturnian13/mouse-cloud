bf17eaf3c377b8a77d03bb2a59d51817f900f987
Chris Rodgers <xrodgers@gmail.com>
2017-05-09 09:28:59 -0400

    update labels

--- make_labels.py
+++ make_labels.py
@@ -89,8 +89,8 @@ def draw_label(label, width, height, obj):
 #~ cage_name_l = ['Purple2', 'Purple3', 'Pink',]
 
 # Directly specify the cages we need
-water_restriction_cage_name_l = ['CR1', 'CR2', 'CR3']
-cage_card_cage_name_l = ['CR1', 'CR2', 'CR3']
+water_restriction_cage_name_l = ['CR6',]
+cage_card_cage_name_l = ['CR6',]
 cage_name_l = water_restriction_cage_name_l + cage_card_cage_name_l
 cage_name_l = list(np.unique(cage_name_l))
     
@@ -125,7 +125,7 @@ sheet = labels.Sheet(specs, draw_label, border=False)
 
 # Define the used labels
 used_labels = []
-for row in range(1, 7): # second number is the 1-based row to start on
+for row in range(1, 4): # second number is the 1-based row to start on
     for col in range(1, 3):
         used_labels.append((row, col))
 sheet.partial_page(1, used_labels)
